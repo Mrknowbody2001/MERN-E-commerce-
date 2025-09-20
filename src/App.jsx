361a335045ee393d52e8340f568546ac6b091d8d
W.A Chamith Sandeepa Thilakarathna <sandeepachamith53@gmail.com>
2025-09-20 14:07:28 +0530

    create pages

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Shop from './pages/Shop'
+
+function App() {
+  return <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/login' element={<Login/>}/>
+    <Route path='/signup' element={<Signup/>}/>
+    <Route path='/shop' element={<Shop/>}/>
+  </Routes>
+  
+  </BrowserRouter>
+}
+export default App
